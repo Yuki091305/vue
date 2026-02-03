11f80f328f11045d419ba994ae87311a2953d4d3
Yuki091305 <yukifieri@gmail.com>
2026-02-03 16:56:01 +0700

    2 практическая

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 let app = new Vue({
    el: '#app',
    data: {
-       product: "Socks"
+       product: "Socks",
+       description: "A pair of warm, fuzzy socks",
+       image: "./assets/vmSocks-green-onWhite.jpg",
+       altText: "A pair of socks",
+       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+      
    }
 })
 
-let app_2 = new Vue({
-    el: '#app_2',
-    data: {
-        description: "A pair of warm, fuzzy socks"
-    }
-})
 
