b539e550fbb11e785c4b0dcaaa1c36fdf0342b8e
Yuki091305 <yukifieri@gmail.com>
2026-02-03 17:15:00 +0700

    3 практическая

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@ let app = new Vue({
        image: "./assets/vmSocks-green-onWhite.jpg",
        altText: "A pair of socks",
        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-      
+       inventory: 100,
+        inStock: true,
+        onSale: true
+
    }
 })
 
