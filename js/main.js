65ffc68be6d710da33323994dd908db7114e664b
Yuki091305 <yukifieri@gmail.com>
2026-02-03 17:46:41 +0700

    4 практическая исправленный немного

--- js/main.js
+++ js/main.js
@@ -10,6 +10,16 @@ let app = new Vue({
         inStock: true,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+   {
+       variantId: 2234,
+       variantColor: 'green'
+   },
+   {
+       variantId: 2235,
+       variantColor: 'blue'
+   }
+],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
    }
 })
