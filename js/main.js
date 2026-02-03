956e92a69c976fdefc8da64d593aaec0e4c7d329
Yuki091305 <yukifieri@gmail.com>
2026-02-03 15:32:05 +0700

    1 практическая

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let app = new Vue({
+   el: '#app',
+   data: {
+       product: "Socks"
+   }
+})
+
+let app_2 = new Vue({
+    el: '#app_2',
+    data: {
+        description: "A pair of warm, fuzzy socks"
+    }
+})
+
