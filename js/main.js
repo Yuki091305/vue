ff3d8b0381471a4ab7cfc4d2b609b8faf90abf07
Yuki091305 <yukifieri@gmail.com>
2026-02-04 15:28:35 +0700

    Практическая 7

--- js/main.js
+++ js/main.js
@@ -4,28 +4,31 @@ let app = new Vue({
        product: "Socks",
         brand: 'Vue Mastery',
        description: "A pair of warm, fuzzy socks",
-       image: "./assets/vmSocks-green-onWhite.jpg",
+       selectedVariant: 0,
        altText: "A pair of socks",
        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-       inventory: 0,
-        inStock: false,
-        onSale: true,
+       inventory: 100,
+        inStock: true,
+        onSale: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
    {
        variantId: 2234,
        variantColor: 'green',
        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+       variantQuantity: 10
    },
    {
        variantId: 2235,
        variantColor: 'blue',
        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+       variantQuantity: 0
    }
 ],
         
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
+
    },
 methods: {
         addToCart() {
@@ -36,10 +39,28 @@ methods: {
             ){
             this.cart -= 1}
         },
-        updateProduct(variantImage) {
-   this.image = variantImage
-}
-
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
+},
+computed: {
+   title() {
+       return this.brand + ' ' + this.product;
+   },
+   image() {
+        return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock(){
+        return this.variants[this.selectedVariant].variantQuantity > 0;
+    },
+    sale(){
+        if(this.onSale){
+            return this.brand + '' + this.product + 'is on sale!';
+        } else {
+            return this.brand + '' + this.product + 'is not on sale.';
+        }
+    }
 },
 })
 
