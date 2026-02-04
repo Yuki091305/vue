5861f4bae19abbcbec1bebfd5052b4900c654cd5
Yuki091305 <yukifieri@gmail.com>
2026-02-04 17:16:41 +0700

    Практическая работа 8

--- js/main.js
+++ js/main.js
@@ -1,37 +1,89 @@
-let app = new Vue({
-   el: '#app',
-   data: {
-       product: "Socks",
+
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template:`
+    <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+    `
+});
+
+Vue.component('product', {
+    props: {
+        premium: {
+           type: Boolean,
+           required: true
+       },
+    },
+   template: `
+   <div class="product">
+       <div class="product-image">
+           <img :src="image" :alt="altText" />
+       </div>
+       <div class="product-info">
+           <h1>{{ title }}</h1>
+            <p>{{ description }}</p>
+            <a :href="link">  More products like this </a>
+            <p v-if="inStock">In Stock</p>
+            <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
+            <p v-else :class="{'line': !inStock}">Out of stock</p>
+            <p>{{ sale }}</p>
+            <p>Shipping: {{ shipping }}</p>
+            <product-details :details = "details"></product-details>
+           <div
+       class="color-box"
+       v-for="(variant, index) in variants"
+       :key="variant.variantId"
+       :style="{ backgroundColor:variant.variantColor }"
+       @mouseover="updateProduct(index)">
+            </div>
+            <ul>
+               <li v-for="sizes in sizes">{{ sizes }}</li>
+            </ul>
+            <div class="cart">
+               <p>Cart({{ cart }})</p>
+            </div>
+            <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">
+               Add to cart
+            </button>
+            <button v-on:click="deleteToCart">Delete to cart</button>
+         </div>
+    </div>
+ `,
+    data() {
+       return {
+        product: "Socks",
         brand: 'Vue Mastery',
-       description: "A pair of warm, fuzzy socks",
-       selectedVariant: 0,
-       altText: "A pair of socks",
-       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-       inventory: 100,
-        inStock: true,
-        onSale: false,
+        description: "A pair of warm, fuzzy socks",
+        selectedVariant: 0,
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inventory: 100,
+        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-   {
-       variantId: 2234,
-       variantColor: 'green',
-       variantImage: "./assets/vmSocks-green-onWhite.jpg",
-       variantQuantity: 10
-   },
-   {
-       variantId: 2235,
-       variantColor: 'blue',
-       variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-       variantQuantity: 0
-   }
-],
-        
+        variants: [{
+            variantId: 2234,
+            variantColor: 'green',
+            variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            variantQuantity: 10 
+        },
+        {
+            variantId: 2235,
+            variantColor: 'blue',
+            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            variantQuantity: 0
+        }],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
-
-   },
-methods: {
-        addToCart() {
+    }
+    },
+   methods: {
+       addToCart() {
             this.cart += 1
         },
         deleteToCart(){
@@ -43,12 +95,12 @@ methods: {
             this.selectedVariant = index;
             console.log(index);
         },
-},
-computed: {
-   title() {
-       return this.brand + ' ' + this.product;
    },
-   image() {
+   computed: {
+    title() {
+       return this.brand + ' ' + this.product;
+    },
+    image() {
         return this.variants[this.selectedVariant].variantImage;
     },
     inStock(){
@@ -60,9 +112,25 @@ computed: {
         } else {
             return this.brand + '' + this.product + 'is not on sale.';
         }
-    }
-},
+    },
+       shipping() {
+        if (this.premium){
+            return "Free";
+        } else {
+            return 2.99
+        }
+    },
+   }
 })
+
+let app = new Vue({
+   el: '#app',
+   data: {
+       premium: true
+   },
+})
+
+
 
 
 
